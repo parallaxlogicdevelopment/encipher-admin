7bfa9c44c95a598002790f465b3242d684c90a6e
mkhemel <hemel878@gmail.com>
2024-09-09 15:50:16 +0600

    Remove all code from repo A

--- src/AppContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-interface AppContextType {
-  restrictBaseUrl: string | string[];
-}
-
-export const AppContext = createContext({});
-
-export const useAppContext = () => useContext(AppContext) as AppContextType;
